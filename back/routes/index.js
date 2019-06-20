88ae5905f37ea9df3aefbf441da21f3e0415a9fd
santiDiazCuru <santidiazcurutchet@gmail.com>
2019-06-20 15:36:52 -0300

    algo de back y categorias provisoriamente como un campo en products

--- back/routes/index.js
+++ back/routes/index.js
@@ -3,13 +3,10 @@ const Router = express.Router();
 const productsRouter = require('./products')
 const usersRouter = require('./users')
 const path = require('path')
-<<<<<<< HEAD
-=======
 const Products = require('../models/Products');
 
 //Router.use('/products', ProductsRouter)
 Router.use('/api/products', ProductsRouter)
->>>>>>> 99d33d405e669e2b389e76a75094be07251269c0
 
 Router.use('/products', productsRouter)
 Router.use('/api/products', productsRouter)
